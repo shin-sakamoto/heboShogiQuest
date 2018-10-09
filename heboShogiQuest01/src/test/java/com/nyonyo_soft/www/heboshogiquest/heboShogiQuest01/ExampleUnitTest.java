a67fde1666185eee9bb11878ff9dffa7fe9f7422
Shin.Sakamoto <walkurerider@gmail.com>
2018-10-09 10:43:39 +0900

    Initial commit

---
+++ heboShogiQuest01/src/test/java/com/nyonyo_soft/www/heboshogiquest/heboShogiQuest01/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.nyonyo_soft.www.heboshogiquest.heboShogiQuest01;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
